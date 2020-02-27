59929f086e8ee710db930a8df87856f74bb827d8
leehyun <leejawa33@gmail.com>
2020-02-27 10:07:00 +0900

    tailwind 적용중

--- plus/resources/views/header.blade.php
+++ plus/resources/views/header.blade.php
@@ -2,16 +2,14 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title><?=$title?></title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="{{mix('css/tailwind.css')}}">
 
     </head>
     <body>
-    <ul>
+
+    <ul >
         <li> <a href="/">메인</a></li>
         <li> <a href="/contact">contact</a></li>
     </ul>
--- plus/resources/views/layout.blade.php
+++ plus/resources/views/layout.blade.php
@@ -1,6 +1,6 @@
 
 
-<table class="table table-striped table-bordered table-hover">
+<table class="bg-green-900">
     <tr>
         <th colspan="23">
             <div >
@@ -30,28 +30,5 @@
         <th>워드프레스</th>
         <th>주식개편</th>
         <th>프로토</th>
-    </tr>
-    <tr>
-        <td><a href="/news/today_list/new_weather">0</a>/<a href="/news/miss_list?category=new_weather" style="color:red;">0</a></td>
-        <td><a href="/news/today_list/movie">2</a>/<a href="/news/miss_list?category=movie" style="color:red;">2</a></td>
-        <td><a href="/news/today_list/exchange">2</a>/<a href="/news/miss_list?category=exchange" style="color:red;">2</a></td>
-        <td><a href="/news/today_list/exchangeMarket">0</a>/<a href="/news/miss_list?category=exchangeMarket" style="color:red;">0</a></td>
-        <td><a href="/news/today_list/game">0</a>/<a href="/news/miss_list?category=game" style="color:red;">0</a></td>
-        <td><a href="/news/today_list/stock">1119</a>/<a href="/news/miss_list?category=stock" style="color:red;">1119</a></td>
-        <td><a href="/news/today_list/stockMarket">180</a>/<a href="/news/miss_list?category=stockMarket" style="color:red;">180</a></td>
-        <td><a href="/news/today_list/oilprice">1</a>/<a href="/news/miss_list?category=oilprice" style="color:red;">1</a></td>
-        <td><a href="/news/today_list/international">1</a>/<a href="/news/miss_list?category=international" style="color:red;">1</a></td>
-        <td><a href="/news/today_list/broadcast">0</a>/<a href="/news/miss_list?category=broadcast" style="color:red;">0</a></td>
-        <td><a href="/news/today_list/goldprice">1</a>/<a href="/news/miss_list?category=goldprice" style="color:red;">1</a></td>
-        <td><a href="/news/today_list/ecos">1</a>/<a href="/news/miss_list?category=ecos" style="color:red;">1</a></td>
-        <td><a href="/news/today_list/bitcoin">4</a>/<a href="/news/miss_list?category=bitcoin" style="color:red;">4</a></td>
-        <td><a href="/news/today_list/robot_editor">1</a>/<a href="/news/miss_list?category=robot_editor" style="color:red;">1</a></td>
-        <td><a href="/news/today_list/lotto">3</a>/<a href="/news/miss_list?category=lotto" style="color:red;">3</a></td>
-        <td><a href="/news/today_list/soccer">0</a>/<a href="/news/miss_list?category=soccer" style="color:red;">0</a></td>
-        <td><a href="/news/today_list/fortune">0</a>/<a href="/news/miss_list?category=fortune" style="color:red;">0</a></td>
-        <td><a href="/news/today_list/crossDramatization">0</a>/<a href="/news/miss_list?category=crossDramatization" style="color:red;">0</a></td>
-        <td><a href="/news/today_list/wordpress">0</a>/<a href="/news/miss_list?category=wordpress" style="color:red;">0</a></td>
-        <td><a href="/news/today_list/NStock">0</a>/<a href="/news/miss_list?category=NStock" style="color:red;">0</a></td>
-        <td><a href="/news/today_list/proto">0</a>/<a href="/news/miss_list?category=proto" style="color:red;">0</a></td>
     </tr>
 </table>
